7ea2562dedb90f2029cfbac566f03c36d21523e9
Mert Cobanov <mertcobanov@gmail.com>
2022-07-01 13:23:14 +0300

    logging function added

--- duplicate.py
+++ duplicate.py
@@ -1,10 +1,18 @@
 import os
+import logging
 import click
 import shutil
 import hashlib
 from collections import defaultdict
 
-folder_path = "./images"
+folder_path = "./json"
+
+logging.basicConfig(
+    filename="history.log", format="%(asctime)s %(message)s", filemode="a"
+)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def _checksum(file_path):
@@ -21,7 +29,7 @@ def scan(folder_path):
     return hashes
 
 
-def fuse(hashes, remove_files=False):
+def fuse(hashes, remove_files=False, save_log=True):
     if not remove_files:
         if not os.path.exists("./duplicates"):
             os.makedirs("./duplicates")
@@ -30,13 +38,26 @@ def fuse(hashes, remove_files=False):
         if len(value) > 1:
             print(f"{key} has more than one file: {value}")
             hashes[key].pop(0)
+
             for file in hashes[key]:
+                file_absolute_path = os.path.join(folder_path, file)
+
                 if remove_files:
-                    os.remove(os.path.join(folder_path, file))
+                    os.remove(file_absolute_path)
+                    if save_log:
+                        logger.info(f"{file_absolute_path} deleted succesfully.")
                 else:
-                    shutil.move(os.path.join(folder_path, file), "./duplicates")
+                    shutil.move(file_absolute_path, "./duplicates")
+                    if save_log:
+                        logger.info(
+                            f"'{file_absolute_path}' moved to './duplicates' directory succesfully."
+                        )
         else:
             print("No duplicates found.")
+            if save_log:
+                logger.info(
+                    f"'{file_absolute_path}' moved to './duplicates' directory succesfully."
+                )
 
 
 def main():
