1c3ece085d9580323270209ccef5d60a6db60591
Mert Cobanov <mertcobanov@gmail.com>
2022-07-01 13:07:39 +0300

    make script functional

--- duplicate.py
+++ duplicate.py
@@ -1,24 +1,48 @@
 import os
+import click
 import shutil
 import hashlib
 from collections import defaultdict
 
 folder_path = "./images"
-files = os.listdir(folder_path)
-hashes = defaultdict(list)
 
-for file in files:
-    hash = hashlib.md5(open(os.path.join(folder_path, file), "rb").read()).hexdigest()
-    hashes[hash].append(file)
 
-for key, value in hashes.items():
-    if len(value) > 1:
+def _checksum(file_path):
+    absolute_path = os.path.join(folder_path, file_path)
+    return hashlib.md5(open(absolute_path, "rb").read()).hexdigest()
+
+
+def scan(folder_path):
+    hashes = defaultdict(list)
+    files = os.listdir(folder_path)
+    for file in files:
+        hash = _checksum(file)
+        hashes[hash].append(file)
+    return hashes
+
+
+def fuse(hashes, remove_files=False):
+    if not remove_files:
         if not os.path.exists("./duplicates"):
             os.makedirs("./duplicates")
-        print(f"{key} has more than one file: {value}")
-        hashes[key].pop(0)
-        for file in hashes[key]:
-            # os.remove(os.path.join(folder_path, file))
-            shutil.move(os.path.join(folder_path, file), "./duplicates")
-    else:
-        print("No duplicates found.")
+
+    for key, value in hashes.items():
+        if len(value) > 1:
+            print(f"{key} has more than one file: {value}")
+            hashes[key].pop(0)
+            for file in hashes[key]:
+                if remove_files:
+                    os.remove(os.path.join(folder_path, file))
+                else:
+                    shutil.move(os.path.join(folder_path, file), "./duplicates")
+        else:
+            print("No duplicates found.")
+
+
+def main():
+    hashes = scan(folder_path)
+    fuse(hashes, remove_files=False)
+
+
+if __name__ == "__main__":
+    main()
